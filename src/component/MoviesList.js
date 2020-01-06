7b5fded4c374c828988217da107346736903abac
Alejandro Vivas <ajvivas86@gmail.com>
2020-01-06 00:24:56 -0300

    Pagination Markup

--- src/component/MoviesList.js
+++ src/component/MoviesList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
+
 import { Movie } from './Movie'
 
 export class MoviesList extends Component {
@@ -14,7 +15,7 @@ export class MoviesList extends Component {
         const { movies, totalResults } = this.props
         return (
             <div className="content__result-row">
-    <p className="content__subtitle slide-in-elliptic-top-fwd">We have found <strong>{totalResults}</strong> coincidences!</p>
+            <p className="content__subtitle slide-in-elliptic-top-fwd">We have found <strong>{totalResults}</strong> coincidences!</p>
                 <div className="MoviesList">
                     {
                         movies.map(movie => {
@@ -31,6 +32,21 @@ export class MoviesList extends Component {
                     }
 
                 </div>
+                {/* <nav className="pagination" role="navigation" aria-label="pagination">
+                    <button className="pagination-previous" title="This is the first page" disabled>Previous</button>
+                    <button className="pagination-next">Next page</button>
+                    <ul className="pagination-list">
+                        <li>
+                        <button className="pagination-link is-current" aria-label="Page 1" aria-current="page">1</button>
+                        </li>
+                        <li>
+                        <button className="pagination-link" aria-label="Goto page 2">2</button>
+                        </li>
+                        <li>
+                        <button className="pagination-link" aria-label="Goto page 3">3</button>
+                        </li>
+                    </ul>
+                </nav> */}
             </div>
         )
 
