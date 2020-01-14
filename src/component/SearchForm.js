ce68f737b2aae2079eef473585058e0fb591d273
Alejandro Vivas <ajvivas86@gmail.com>
2020-01-14 00:49:33 -0300

    Query Search

--- src/component/SearchForm.js
+++ src/component/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import dataService from '../services/data.service'
 
 
@@ -13,24 +13,34 @@ export const SearchForm = props => {
 
     const handleSubmit = e => {
         e.preventDefault()
-
         if (!inputMovie || inputMovie.toString().trim().length === 0)
             return
+        window.location.href = "/?s=" + inputMovie;
+    }
 
-        setLoading(true)
-        setErrors(false)
-
-        dataService.searchMovies(inputMovie.trim()).then(data => {
-            const { Search = [], totalResults = "" } = data;
-            props.onResults({ Search, totalResults, inputMovie })
-            setLoading(false)
-        }).catch(error => {
-            console.log('Error: ', error)
-            setLoading(false)
-            setErrors(true)
-        })
+    const searchMovie = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const search = urlParams.get('s');
+        const page = urlParams.get('page');
+        if (search) {
+            setLoading(true)
+            setErrors(false)
+            dataService.searchMovies(search.trim(), page).then(data => {
+                const { Search = [], totalResults = "" } = data;
+                props.onResults({ Search, totalResults, inputMovie: search })
+                setLoading(false)
+            }).catch(error => {
+                console.log('Error: ', error)
+                setLoading(false)
+                setErrors(true)
+            })
+        }
 
     }
+
+    useEffect(() => {
+        searchMovie()
+    }, [])
 
     return (
         <form onSubmit={handleSubmit}>
--- src/services/data.service.js
+++ src/services/data.service.js
@@ -2,7 +2,7 @@ const API_URL = "https://www.omdbapi.com"
 const API_KEY = "a4766de2"
 
 export default {
-    async searchMovies(keyword = "", page = null) {
+    async searchMovies(keyword = "", page = 1) {
         try {
             const res = await fetch(`${API_URL}/?apikey=${API_KEY}&s=${keyword}&page=${page}`)
             const data = await res.json()
