c3415d3bb6a8079e880b56f1d825553b50ef6515
Alejandro Vivas <alejandrojv17@gmail.com>
2020-04-27 11:28:47 -0400

    Improve UX, Refactoring, better use of router hooks

--- src/reducers/loading.js
+++ src/reducers/loading.js
@@ -1,7 +1,6 @@
 export const loadingReducer = (state = false, action) => {
     switch (action.type) {
         case "LOADING":
-            console.log(action.payload)
             return state = action.payload
         default:
             return state
